42457fb2c8ca64f515b2f79930b1e5eb20543669
apurbojamananis <apurbojaman.anis@gmail.com>
2023-05-19 22:48:55 +0600

    added mongodb

--- index.js
+++ index.js
@@ -11,6 +11,42 @@ app.use(express.json());
 
 // Mongodb
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.orqgdcn.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const toysCollection = client.db("toyShop").collection("toys");
+
+    app.post("/allToys", async (req, res) => {
+      const toys = req.body;
+      console.log(toys);
+      const result = await toysCollection.insertOne(toys);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("toyShop").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Toy Shop is running...");
